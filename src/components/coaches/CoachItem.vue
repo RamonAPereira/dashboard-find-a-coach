af2ceb87ba5d39023ab2912b6aa312b479a1041b
RamonAPereira <ramonpereiranzd@gmail.com>
2022-05-07 15:10:59 -0300

    feat(coach list): usuario pode ver a lista de coaches
    coachList e coachItem logic

---
+++ src/components/coaches/CoachItem.vue
@@ -0,0 +1,51 @@
+<script lang="ts" setup>
+import { computed } from "@vue/reactivity";
+import { PropType } from "vue";
+import Coach from "../../types/index";
+
+const props = defineProps({
+  coach: {
+    required: true,
+    type: Object as PropType<Coach>,
+  },
+});
+
+const fullName = computed(
+  () => `${props.coach.firstName} ${props.coach.lastName}`
+);
+const paymentHour = computed(() => `${props.coach.hourlyRates}/hours`);
+const coachContactLink = computed(() => `/coaches/${props.coach.id}/contact`);
+const coachDetailsLink = computed(() => `/coaches/${props.coach.id}`);
+</script>
+
+<template>
+  <li class="coach">
+    <h3 class="coach__name">{{ fullName }}</h3>
+    <h4 class="coach__payment">{{ paymentHour }}</h4>
+    <ul>
+      <li v-for="(area, index) in props.coach.areas" :key="index">
+        {{ area }}
+      </li>
+    </ul>
+    <div class="coach__controls">
+      <router-link :to="coachDetailsLink">Coach Details</router-link>
+      <router-link :to="coachContactLink">Contact</router-link>
+    </div>
+  </li>
+</template>
+
+<style scoped>
+.coach {
+  @apply border border-black rounded-xl p-4 w-full;
+}
+.coach__name {
+  @apply font-bold text-2xl;
+}
+.coach__payment {
+  @apply font-bold;
+}
+
+.coach__controls {
+  @apply flex  justify-end;
+}
+</style>
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,4 +1,34 @@
+<script lang="ts" setup>
+import { computed } from "@vue/reactivity";
+import { useCoaches } from "../../store/coaches";
+import CoachItem from "../../components/coaches/CoachItem.vue";
+import Coach from "../../types";
+
+const coaches = useCoaches();
+
+const displayCoaches = computed(() => coaches.hasCoaches);
+</script>
+
 <template>
   <section>FILTER</section>
-  <section>LIST OF COACHES</section>
+  <section>
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register as Coach</router-link>
+    </div>
+    <ul class="coach__list" v-if="displayCoaches">
+      <CoachItem
+        v-for="(coach, index) in displayCoaches"
+        :key="index"
+        :coach="coach"
+      />
+    </ul>
+    <p v-else>No Coaches Found</p>
+  </section>
 </template>
+
+<style scoped>
+.coach__list {
+  @apply flex flex-col justify-center items-center gap-4 px-2 my-4;
+}
+</style>
